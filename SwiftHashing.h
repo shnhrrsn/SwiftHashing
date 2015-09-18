af6256609a7da50ed6bd90b27aa09af033339982
shnhrrsn <shnhrrsn@gmail.com>
2015-09-18 05:03:58 -0400

    Added nonnull

--- SwiftHashing.h
+++ SwiftHashing.h
@@ -7,5 +7,9 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 NSString* hmacHash(NSString* secret, NSString* string);
 NSString* md5Hash(NSString* string);
+
+NS_ASSUME_NONNULL_END
